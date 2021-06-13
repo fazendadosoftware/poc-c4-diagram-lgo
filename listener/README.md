5ae62743e98df90a561530608f867f73745d76ee
Paulo dos Santos <paulo@motovalor.com>
2021-06-13 20:17:08 -0300

    Added basic auth

--- listener/README.md
+++ listener/README.md
@@ -4,12 +4,14 @@
 The following Application Settings must be defined in your Azure deployment:
 * LEANIX_INSTANCE
 * LEANIX_APITOKEN
+* LEANIX_USERNAME (for basic auth)
+* LEANIX_PASSWORD (for basic auth)
 
-### Example trigger command:
+### Example trigger command (with basic authentication, username/password = LEANIX/LEANIX):
 ```bash
-curl -X POST -H "Content-Type: application/json" -d @FILENAME DESTINATION
+curl -X POST -H "Content-Type: application/json" -d @FILENAME DESTINATION -u "username:password"
 ```
 
 ```bash
-curl -X POST -H "Content-Type: application/json" -d @./test.json http://localhost:7071/api/listener
+curl -X POST -H "Content-Type: application/json" -d @./test.json http://localhost:7071/api/listener -u "LEANIX:LEANIX"
 ```
--- listener/index.js
+++ listener/index.js
@@ -1,8 +1,22 @@
+const auth = require('basic-auth')
 const { getAccessToken, upsertBookmark, processBody, fetchBookmarks } = require('./businessLogic')
+const { LEANIX_USERNAME: username = null, LEANIX_PASSWORD: password = null } = process.env
+
+let requests = 0
 
 module.exports = async function (context, req) {
+  requests++
+  if (username === null || password === null) context.warn('APP Settings "LEANIX_USERNAME" and "LEANIX_PASSWORD" should be set for basic auth of POST requests')
   const { method, headers, body } = req
-  const { 'content-type': contentType = null } = headers
+  const { 'content-type': contentType = null, 'x-forwarded-for': forwardedFor } = headers
+  if (username !== null && password !== null) {
+    const { name: providedUsername = null, pass: providedPassword = null } = auth(req) || {}
+      if (providedUsername !== username || providedPassword !== password) {
+      context.res = { status: 403, body: 'invalid credentials' }
+      context.error(new Date().toISOString(), '403 Forbidden', forwardedFor)
+      return
+    }
+  }
   if (method === 'POST') {
     if (contentType !== 'application/json') {
       context.res = { status: 415, body: `invalid content-type ${contentType}` }
@@ -25,5 +39,8 @@ module.exports = async function (context, req) {
         body: error.message || 'an error occurred'
       }
     }
+  }
+  context.res = {
+    body: `${new Date().toISOString()} ===> #${requests}: `
   }
 }
